9f72dbe1f54fced7e78f5fdc1fbb258f8557626d
Choi Byungyoon <byungyoonc@gmail.com>
2016-02-24 20:06:19 +0900

    Added Lightpath to solve the light path using RK4 for 2nd order ODE

---
+++ include/Lightpath.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Medium.h"
+#include <vector>
+
+class Lightpath
+{
+public:
+    Lightpath(Medium *_pMedium);
+    virtual ~Lightpath();
+
+    void solve(double s_0, double step, double s_max, glm::dvec3 x_0, glm::dvec3 w_0);
+
+protected:
+    inline glm::dvec3 dwds(glm::dvec3 x, glm::dvec3 w);
+
+    std::vector<std::pair<double, glm::dvec3>> path;
+    Medium *pMedium;
+};
---
+++ src/Lightpath.cpp
@@ -0,0 +1,45 @@
+#include "Lightpath.h"
+#include <glm/glm.hpp>
+
+Lightpath::Lightpath(Medium *_pMedium) : pMedium(_pMedium)
+{
+}
+
+Lightpath::~Lightpath()
+{
+}
+
+// Solving numerically using RK4 for 2nd order ODE
+void Lightpath::solve(double s_0, double step, double s_max, glm::dvec3 x_0, glm::dvec3 w_0)
+{
+    path.clear();
+
+    glm::dvec3 dx1, dx2, dx3, dx4, dw1, dw2, dw3, dw4, dx, dw;
+    glm::dvec3 x, w;
+
+    x = x_0;
+    w = w_0;
+
+    for (double s = s_0; s < s_max; s += step)
+    {
+        path.push_back(std::pair<double, glm::dvec3>(s, x));
+        dx1 = step * w;
+        dw1 = step * dwds(x, w);
+        dx2 = step * (w + dw1 / 2.0);
+        dw2 = step * dwds(x + dx1 / 2.0, w + dw1 / 2.0);
+        dx3 = step * (w + dw2 / 2.0);
+        dw3 = step * dwds(x + dx2 / 2.0, w + dw2 / 2.0);
+        dx4 = step * (w + dw3);
+        dw4 = step * dwds(x + dx3, w + dw3);
+        dx = (dx1 + 2.0 * dx2 + 2.0 * dx3 + dx4) / 6.0;
+        dw = (dw1 + 2.0 * dw2 + 2.0 * dw3 + dw4) / 6.0;
+        x += dx;
+        w += dw;
+    }
+}
+
+// [Harris 1965; Born and Wolf 1999; Pomraning 2005]
+inline glm::dvec3 Lightpath::dwds(glm::dvec3 x, glm::dvec3 w)
+{
+    return (pMedium->gradient(x) - glm::dot(w, pMedium->gradient(x)) * w) / pMedium->f(x);
+}
